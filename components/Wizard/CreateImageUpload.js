ad0d81315132c854e3f7af4b7198f259541d9e1a
Jacob Kozol <jacobdkozol@gmail.com>
2021-04-19 17:26:12 +0200

    wizard: update rhel-edge name to edge
    "rhel-edge-*" image types have been renamed to "edge-*" for RHEL 8.5 and
    up.

--- components/Wizard/CreateImageUpload.js
+++ components/Wizard/CreateImageUpload.js
@@ -327,9 +327,9 @@ class CreateImageUploadModal extends React.Component {
     if (
       imageType === "" ||
       imageType === "fedora-iot-commit" ||
-      imageType === "rhel-edge-commit" ||
-      imageType === "rhel-edge-container" ||
-      imageType === "rhel-edge-installer"
+      imageType === "edge-commit" ||
+      imageType === "edge-container" ||
+      imageType === "edge-installer"
     ) {
       return false;
     }
@@ -351,7 +351,7 @@ class CreateImageUploadModal extends React.Component {
 
   isValidOstree() {
     if (this.state.ostreeSettings.parent && this.state.ostreeSettings.url) return false;
-    if (this.state.imageType === "rhel-edge-installer" && !this.state.ostreeSettings.url) return false;
+    if (this.state.imageType === "edge-installer" && !this.state.ostreeSettings.url) return false;
     return this.isValidOstreeRef(this.state.ostreeSettings.ref);
   }
 
